b7fc97ff3466aa5c5a658f86f6cf3cf308d035ca
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2013-07-24 18:35:41 +0200

    Started #8 by adding empty SDK library

--- src/Canister.Sdk/Properties/AssemblyInfo.cs
+++ src/Canister.Sdk/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Canister")]
+[assembly: AssemblyTitle("Canister.Sdk")]
 [assembly: AssemblyDescription("")]
 
 [assembly: ComVisible(false)]
